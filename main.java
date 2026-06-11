eb1274231d35b2219e432567d1f72b5b6cd2713e
Walter1215 <wcuad002@fiu.edu>
2026-06-10 21:48:50 -0400

    Add menu system for student management

--- main.java
+++ main.java
@@ -1,49 +1,72 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
 
-        // Create first student 
-        Student student1 = new Student(
-                "Walter",
-                1,
-                "Computer Science"
-        );
-
-        Student student2 = new Student(
-                "Lucia",
-                2,
-                "Early Education"
-        );
-
-        Student student3 = new Student(
-                "Joelito",
-                3,
-                "Middle School"
-        );
-        
-        // Create ArrayList
         ArrayList<Student> students = new ArrayList<>();
 
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i < students.size(); i++) {
+        while (true) {
 
-            System.out.println("Student Info:");
-            System.out.println("----------------");
+            System.out.println("\nStudent Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
 
-            System.out.println(students.get(i).name);
-            System.out.println(students.get(i).id);
-            System.out.println(students.get(i).major);
-            System.out.println("----------------");
-            
+            int choice = input.nextInt();
+            input.nextLine();
 
-            System.out.print(" ");
-        
+            if (choice == 1) {
+
+                System.out.print("Enter Student Name: ");
+                String name = input.nextLine();
+
+                System.out.print("Enter Student ID: ");
+                int id = input.nextInt();
+                input.nextLine();
+
+                System.out.print("Enter Major: ");
+                String major = input.nextLine();
+
+                Student student = new Student(
+                        name,
+                        id,
+                        major
+                );
+
+                students.add(student);
+
+                System.out.println("Student Added!");
+
+            } else if (choice == 2) {
+
+                System.out.println("\nStudent Records");
+                System.out.println("----------------");
+
+                for (int i = 0; i < students.size(); i++) {
+
+                    System.out.println("Name: " + students.get(i).name);
+                    System.out.println("ID: " + students.get(i).id);
+                    System.out.println("Major: " + students.get(i).major);
+                    System.out.println("----------------");
+                }
+
+            } else if (choice == 3) {
+
+                System.out.println("Exiting...");
+                break;
+
+            } else {
+
+                System.out.println("Invalid option.");
+
+            }
         }
 
+        input.close();
     }
-    
 }
