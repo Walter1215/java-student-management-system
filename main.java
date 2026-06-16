d0b1be65c3308f40472c3f230b321eada09ab41b
Walter1215 <wcuad002@fiu.edu>
2026-06-15 21:54:05 -0400

    Complete CRUD menu for student management system

--- main.java
+++ main.java
@@ -6,7 +6,6 @@ public class main {
     public static void main(String[] args) {
 
         ArrayList<Student> students = new ArrayList<>();
-
         Scanner input = new Scanner(System.in);
 
         while (true) {
@@ -14,7 +13,10 @@ public class main {
             System.out.println("\nStudent Management System");
             System.out.println("1. Add Student");
             System.out.println("2. View Students");
-            System.out.println("3. Exit");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
             System.out.print("Choose an option: ");
 
             int choice = input.nextInt();
@@ -32,12 +34,7 @@ public class main {
                 System.out.print("Enter Major: ");
                 String major = input.nextLine();
 
-                Student student = new Student(
-                        name,
-                        id,
-                        major
-                );
-
+                Student student = new Student(name, id, major);
                 students.add(student);
 
                 System.out.println("Student Added!");
@@ -48,7 +45,6 @@ public class main {
                 System.out.println("----------------");
 
                 for (int i = 0; i < students.size(); i++) {
-
                     System.out.println("Name: " + students.get(i).name);
                     System.out.println("ID: " + students.get(i).id);
                     System.out.println("Major: " + students.get(i).major);
@@ -57,13 +53,68 @@ public class main {
 
             } else if (choice == 3) {
 
+                System.out.print("Enter Student Name: ");
+                String searchName = input.nextLine();
+
+                for (int i = 0; i < students.size(); i++) {
+
+                    if (students.get(i).name.equalsIgnoreCase(searchName)) {
+                        System.out.println("\nStudent Found");
+                        System.out.println("----------------");
+                        System.out.println("Name: " + students.get(i).name);
+                        System.out.println("ID: " + students.get(i).id);
+                        System.out.println("Major: " + students.get(i).major);
+                        System.out.println("----------------");
+                        break;
+                    }
+                }
+
+            } else if (choice == 4) {
+
+                System.out.print("Enter Student Name to Update: ");
+                String updateName = input.nextLine();
+
+                for (int i = 0; i < students.size(); i++) {
+
+                    if (students.get(i).name.equalsIgnoreCase(updateName)) {
+
+                        System.out.print("Enter New Name: ");
+                        students.get(i).name = input.nextLine();
+
+                        System.out.print("Enter New ID: ");
+                        students.get(i).id = input.nextInt();
+                        input.nextLine();
+
+                        System.out.print("Enter New Major: ");
+                        students.get(i).major = input.nextLine();
+
+                        System.out.println("Student Updated!");
+                        break;
+                    }
+                }
+
+            } else if (choice == 5) {
+
+                System.out.print("Enter Student Name to Delete: ");
+                String deleteName = input.nextLine();
+
+                for (int i = 0; i < students.size(); i++) {
+
+                    if (students.get(i).name.equalsIgnoreCase(deleteName)) {
+                        students.remove(i);
+                        System.out.println("Student Deleted!");
+                        break;
+                    }
+                }
+
+            } else if (choice == 6) {
+
                 System.out.println("Exiting...");
                 break;
 
             } else {
 
                 System.out.println("Invalid option.");
-
             }
         }
 
